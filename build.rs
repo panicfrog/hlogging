6c97c515b7aabc4580998cd0835304af0e34857f
yeyongping <burnedfrog@163.com>
2021-10-01 15:02:30 +0800

    generate header file

--- build.rs
+++ build.rs
@@ -10,6 +10,7 @@ fn main() {
 
     let package_name = env::var("CARGO_PKG_NAME").unwrap();
     let output_file = target_dir()
+        .join("include")
         .join(format!("{}.h", package_name))
         .display()
         .to_string();
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 use std::ffi::CStr;
 use std::os::raw::{c_int, c_char};
-use std::fs::{OpenOptions};
+use std::fs::{OpenOptions, File};
 use std::io::Write;
+use std::path::Path;
 
 #[cfg(test)]
 mod tests {
@@ -12,35 +13,35 @@ mod tests {
 }
 
 #[no_mangle]
-pub extern "C" fn write_log_file(path_str:*const c_char, message: *const c_char) -> c_int {
+pub extern "C" fn write_log_file(file_path: *const c_char, message: *const c_char) -> c_int {
     let file_path = unsafe {
-        assert!(!path_str.is_null());
-        CStr::from_ptr(path_str)
+        assert!(!file_path.is_null());
+        CStr::from_ptr(file_path)
     };
     let message = unsafe {
         assert!(!message.is_null());
         CStr::from_ptr(message)
     };
+
     let file_path = file_path.to_str().expect("path error");
     let message = message.to_str().expect("message error");
+
     let file = OpenOptions::new()
         .write(true)
         .create(true)
         .append(true)
         .open(file_path);
     match file {
+        Ok(mut f) => match f.write(message.as_bytes()) {
+            Ok(_) => 0,
+            Err(e) => {
+                dbg!("file: {} error: {:?}", file_path, );
+                2
+            },
+        },
         Err(e) => {
-            dbg!("{:?}", e);
+            dbg!("file: {} error: {:?}", file_path, );
             1
         },
-        Ok(mut f) => {
-           match f.write(message.as_bytes()) {
-               Err(e) => {
-                   dbg!("{:?}", e);
-                   2
-               },
-               Ok(_) => 0
-           }
-        }
     }
 }
